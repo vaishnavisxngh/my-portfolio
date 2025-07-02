912c0740349e376eb144f915f9f1a915fb20996e
vaishnavisxngh <vaishnavi.29.singh@gmail.com>
2025-07-02 14:55:59 +0530

    commit

---
+++ app/components/common/MusicToggle.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react";
+import { isMobile } from "react-device-detect";
+import { usePortalStore } from "@stores";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const MusicToggle = () => {
+  const musicToggleRef = useRef<HTMLDivElement>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const isActive = usePortalStore((state) => state.activePortalId);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  useGSAP(() => {
+    gsap.to(musicToggleRef.current, {
+      opacity: isActive ? 0 : 1,
+      duration: 1,
+      delay: isActive ? 0 : 1,
+    });
+  }, [isActive]);
+
+  const toggleMusic = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    if (audio.paused) {
+      audio.play();
+      setIsPlaying(true);
+    } else {
+      audio.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  return (
+    <>
+      <audio
+        ref={audioRef}
+        src="/icons/audio.mp3"
+        autoPlay
+        loop
+        preload="auto"
+      />
+      <div
+        ref={musicToggleRef}
+        style={{
+          position: "fixed",
+          top: isMobile ? "2.5rem" : "4rem",
+          right: "1.5rem",
+          opacity: 0,
+          zIndex: 2,
+        }}
+      >
+        <button
+          onClick={toggleMusic}
+          className="px-4 py-2 rounded-md bg-black/60 text-white hover:bg-black/80 transition shadow-md shadow-black/20 ">
+          {isPlaying ? "Pause Music" : "Play Music"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default MusicToggle;
---
+++ app/constants/footer.ts
@@ -0,0 +1,34 @@
+import { FooterLink } from "../types/footer";
+
+export const FOOTER_LINKS: FooterLink[] = [
+  {
+    name: 'LinkedIn',
+    hoverText: 'Connect with me',
+    icon: 'icons/linkedin.svg',
+    url: 'https://www.linkedin.com/in/vaishnavi-singh-3923a328a/',
+  },
+  {
+    name: 'GitHub',
+    hoverText: 'Trying to keep up',
+    icon: 'icons/github.svg',
+    url: 'https://github.com/vaishnavisxngh',
+  },
+  {
+    name: 'Spotify',
+    hoverText: 'Old ass choices',
+    icon: 'icons/spotify.svg',
+    url: 'https://open.spotify.com/user/21hr4w2hzp4ceidewwsb4bxoy',
+  },
+  {
+    name: 'Instagram',
+    hoverText: '@vaishnavisxngh',
+    icon: 'icons/instagram.svg',
+    url: 'https://www.instagram.com/clevirli/',
+  },
+  {
+    name: 'Resume',
+    hoverText: 'Download',
+    icon: 'icons/file.svg',
+    url: './Vaishnavi-Singh Resume.pdf',
+  }
+];
